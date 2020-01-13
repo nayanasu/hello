bf8f2510c721e96f27abcd590c42841731a94e82
nayanasu <59083161+nayanasu@users.noreply.github.com>
2020-01-13 16:32:03 +0530

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,10 +1,8 @@
+public class hello {
 
-public class hello{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
 
-public ststic void main(){
-
-
-System.out.println("HelloWorld");
-
-}
 }
